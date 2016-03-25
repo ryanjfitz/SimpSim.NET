c462d02bdf29f0b9d7555aaa73acca9611f1b7fa
Ryan Fitzgerald <ryanjfitz@gmail.com>
2016-03-24 21:02:56 -0500

    Add test for AssemblyEditorWindowViewModel.

---
+++ SimpSim.NET.Presentation.Tests/AssemblyEditorWindowViewModelTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using SimpSim.NET.Presentation.ViewModels;
+
+namespace SimpSim.NET.Presentation.Tests
+{
+    [TestFixture]
+    public class AssemblyEditorWindowViewModelTests
+    {
+        [Test]
+        public void AssembleCommandShouldAssembleInstructionsToMemory()
+        {
+            AssemblyEditorWindowViewModel assemblyEditorWindowViewModel = new AssemblyEditorWindowViewModel(null);
+
+            assemblyEditorWindowViewModel.AssemblyEditorText = SamplePrograms.HelloWorldCode;
+
+            assemblyEditorWindowViewModel.AssembleCommand.Execute(null);
+
+            var expectedBytes = SamplePrograms.HelloWorldInstructions.SelectMany(instruction => instruction.Bytes).ToList();
+
+            var actualBytes = new List<byte>();
+
+            for (byte address = 0; address < expectedBytes.Count; address++)
+                actualBytes.Add(ModelSingletons.Memory[address]);
+
+            CollectionAssert.AreEqual(expectedBytes, actualBytes);
+        }
+    }
+}
