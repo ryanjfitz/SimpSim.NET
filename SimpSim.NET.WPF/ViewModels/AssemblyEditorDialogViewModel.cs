4d92399be964bf0ea3ad863da6f0b168beada513
Ryan Fitzgerald <ryanjfitz@gmail.com>
2020-08-17 10:26:24 -0500

    Prevent unused event compiler warning.

--- SimpSim.NET.WPF/ViewModels/AssemblyEditorDialogViewModel.cs
+++ SimpSim.NET.WPF/ViewModels/AssemblyEditorDialogViewModel.cs
@@ -73,6 +73,6 @@ namespace SimpSim.NET.WPF.ViewModels
 
         public string Title => "Assembly Editor";
 
-        public event Action<IDialogResult> RequestClose;
+        public event Action<IDialogResult> RequestClose { add{} remove{} }
     }
 }
