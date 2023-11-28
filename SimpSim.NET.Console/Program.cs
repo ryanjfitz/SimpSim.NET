2280fe3def218fd673f1db1d4cc93a687eaca3d4
Ryan Fitzgerald <ryanjfitz@gmail.com>
2023-11-28 10:03:56 -0600

    Convert console app to top level program.

--- SimpSim.NET.Console/Program.cs
+++ SimpSim.NET.Console/Program.cs
@@ -1,66 +1,51 @@
-﻿namespace SimpSim.NET.Console;
+﻿using SimpSim.NET;
 
-class Program
+string assemblyCode = null;
+
+switch (args.Length)
 {
-    static async Task Main(string[] args)
-    {
-        string assemblyCode = null;
-
-        switch (args.Length)
+    case 0:
+        do
         {
-            case 0:
-                bool isValidSelection;
+            Console.WriteLine("Choose a sample program to execute:\n");
+            var menuItems = SamplePrograms.List.OrderBy(sp => sp.Value).Select(sp => $"{sp.Value}) {sp.Name}");
+            Console.WriteLine(string.Join(Environment.NewLine, menuItems));
 
-                do
-                {
-                    System.Console.WriteLine("Choose a sample program to execute:\n");
-                    foreach (var sampleProgram in SamplePrograms.List.OrderBy(sp => sp.Value))
-                        System.Console.WriteLine($"{sampleProgram.Value}) {sampleProgram.Name}");
+            if (int.TryParse(Console.ReadLine(), out int selection) && SamplePrograms.TryFromValue(selection, out var sampleProgram))
+                assemblyCode = sampleProgram.Code;
 
-                    isValidSelection = int.TryParse(System.Console.ReadLine(), out int selection);
+            if (assemblyCode == null)
+                Console.WriteLine("Invalid selection. Please choose again.\n");
+        } while (assemblyCode == null);
 
-                    if (SamplePrograms.TryFromValue(selection, out var result))
-                        assemblyCode = result.Code;
-                    else
-                        isValidSelection = false;
-
-                    if (!isValidSelection)
-                        System.Console.WriteLine("Invalid selection. Please choose again.\n");
-                } while (!isValidSelection);
-
-                break;
-            case 1:
-                string file = args[0];
-                try
-                {
-                    assemblyCode = await File.ReadAllTextAsync(file);
-                }
-                catch (Exception ex)
-                {
-                    System.Console.WriteLine($"Unable to read file \"{file}\": {ex.Message}");
-                }
-
-                break;
-            default:
-                System.Console.WriteLine("Usage:");
-                System.Console.WriteLine("  SimpSim.NET.Console [<assembly-file>]");
-                break;
+        break;
+    case 1:
+        string file = args[0];
+        try
+        {
+            assemblyCode = await File.ReadAllTextAsync(file);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Unable to read file \"{file}\": {ex.Message}");
         }
 
-        if (assemblyCode != null)
-            await RunProgram(assemblyCode);
-    }
+        break;
+    default:
+        Console.WriteLine("Usage:");
+        Console.WriteLine("  SimpSim.NET.Console [<assembly-file>]");
+        break;
+}
 
-    private static async Task RunProgram(string assemblyCode)
-    {
-        SimpleSimulator simulator = new SimpleSimulator();
+if (assemblyCode != null)
+{
+    SimpleSimulator simulator = new SimpleSimulator();
 
-        Instruction[] instructions = simulator.Assembler.Assemble(assemblyCode);
+    Instruction[] instructions = simulator.Assembler.Assemble(assemblyCode);
 
-        simulator.Memory.LoadInstructions(instructions);
+    simulator.Memory.LoadInstructions(instructions);
 
-        simulator.Registers.ValueWrittenToOutputRegister += System.Console.Write;
+    simulator.Registers.ValueWrittenToOutputRegister += Console.Write;
 
-        await simulator.Machine.RunAsync();
-    }
+    await simulator.Machine.RunAsync();
 }
