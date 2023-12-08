8f0003f301a57fc8092bc1fa86a9a3c93fe2aebb
Ryan Fitzgerald <ryanjfitz@gmail.com>
2023-12-08 11:15:38 -0600

    Add unit test for sample programs.

---
+++ SimpSim.NET.Tests/SampleProgramsTests.cs
@@ -0,0 +1,16 @@
+﻿using Xunit;
+
+namespace SimpSim.NET.Tests;
+
+public class SampleProgramsTests
+{
+    [Fact]
+    public void Sample_programs_should_have_consecutive_values_starting_with_1()
+    {
+        var expected = Enumerable.Range(1, SamplePrograms.List.Count);
+
+        var actual = SamplePrograms.List.Select(sp => sp.Value);
+
+        Assert.Equal(expected, actual);
+    }
+}
